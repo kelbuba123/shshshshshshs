abe578323ae4f217824a0e87af5572336f1f496f
kelbuba123 <43009695+kelbuba123@users.noreply.github.com>
2018-09-07 17:25:09 +0300

    Update balance.js

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -21,6 +21,6 @@ module.exports.run = function(bot, command, args, message){
 
 module.exports.help = {
     names: ['balance'],
-    usage: 'b-8ball <question>',
+    usage: 's!8ball <question>',
     description: 'Ask a question...'
 }
